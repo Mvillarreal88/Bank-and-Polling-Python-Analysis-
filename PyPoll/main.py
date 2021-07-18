ae0d2864539db827035fd88a9f8fe6c9e87c0c47
Matthew Villarreal <vmatthew360@gmail.com>
2021-07-18 13:13:05 -0500

    Added the winner variables as well as printed results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,6 +13,7 @@ with open(election_csvpath) as ele_file:
     header = next(csvreader)
 
 
+    #Assighning variables
     Total_votes = 0
     candidate_list = []
     khan_votes = 0
@@ -20,6 +21,8 @@ with open(election_csvpath) as ele_file:
     otooley_votes = 0
     correy_votes = 0
 
+
+    #Using If and Elif to add to variables
     for row in csvreader:
         Total_votes += 1
 
@@ -39,10 +42,32 @@ with open(election_csvpath) as ele_file:
             otooley_votes += 1
 
 
+    #Using a dictionary to add names of the candidates to their total votes received
+
+    candidate_dict = {}
+
+    candidate_dict["Khan"] = khan_votes
+    candidate_dict["Li"] = li_votes
+    candidate_dict["O'Tooley"] = otooley_votes
+    candidate_dict["Correy"] = correy_votes
+
     #Calculate percentages for each candidate
 
-    per_Khan = round(khan_votes/Total_votes * 100)
-    per_li = round(li_votes/Total_votes * 100) 
-    per_otooley = round(otooley_votes/Total_votes * 100)
-    per_correy = round(correy_votes/Total_votes * 100)
+    perc_Khan = round(khan_votes/Total_votes * 100)
+    perc_li = round(li_votes/Total_votes * 100) 
+    perc_otooley = round(otooley_votes/Total_votes * 100)
+    perc_correy = round(correy_votes/Total_votes * 100)
 
+    Winner = max(candidate_dict, key= candidate_dict.get)
+
+    #Printing the results of the election
+    print(["Election Results"])
+    print(["---------------------"])
+    print("Total Votes: " + Total_votes)
+    print(["---------------------"])
+    print([f"Khan: {perc_Khan}% ({khan_votes})"])
+    print([f"Li: {perc_li}% ({li_votes})"])
+    print([f"Correy: {perc_correy}% ({correy_votes})"])
+    print([f"O'Tooley: {perc_otooley}% ({otooley_votes})"])
+    print(["---------------------"])
+    print("Winner: " + Winner)
